6e7b4b5340ea3adb91ed5358db7d84d05a89f2f5
ChiragMulla <75091093+ChiragMulla@users.noreply.github.com>
2020-11-26 23:22:38 +0530

    Update TechnicalBlogApplication.java
    just added a comment

--- TechnicalBlogApplication-Chirag/src/main/java/technicalblog/TechnicalBlogApplication.java
+++ TechnicalBlogApplication-Chirag/src/main/java/technicalblog/TechnicalBlogApplication.java
@@ -2,7 +2,7 @@ package technicalblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// this is the change
 @SpringBootApplication
 public class TechnicalBlogApplication {
 
